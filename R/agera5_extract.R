9abc72d4e8a95f5fe5025eb6b786d98ecfa3be0c
David Brown <50154982+dbrownf@users.noreply.github.com>
2020-06-13 08:53:02 -0600

    Clean code
    Cleaned code for readability

--- R/agera5_extract.R
+++ R/agera5_extract.R
@@ -18,7 +18,8 @@
 #'
 #'
 #'
-#'@param agera5_folder a string with the path for the local data of AgERA5
+#'@param .agera5_folder a string with the path for the local data of AgERA5
+#'downloaded files should be stored per years
 #'
 #'@param .date
 #'Format dd-mm-YYYY
@@ -46,44 +47,67 @@
 #"holds the path for the local AgERA5 data folder"
 #agera5_folder<-""
 
-#'set AgERA5 data folder
-#'example "F:/environmental_data/AgERA5"
-set_agera5_source<-function(.folder_path){
-  agera5_folder<-.folder_path
-}
-
-
-#'
+#deprecated - folder is no passed to each function to avoid global variables
+#set AgERA5 data folder
+#example "F:/environmental_data/AgERA5"
+# set_agera5_source<-function(.folder_path){
+#   agera5_folder<-.folder_path
+# }
+#
 
 #'extracts data for a given location and date
-extract_single_date<-function(.date,.location_xy,.var,.statistic){
-  file_path<-get_filepath(.var,.statistic,.date)
-  agera5_brick<-raster::brick(file_path)
-  data_out<-raster::extract(agera5_brick,.location_xy)
-  date_to_extract<-gsub(x=.date,pattern = "-",replacement = ".")
-  date_to_extract<-paste0("X",date_to_extract)
-  extracted_data<-data_out[1,date_to_extract]
+extract_single_date <- function(.agera5_folder,
+                                .date,.location_xy,
+                                .var,.statistic){
+
+  file_path <- get_filepath(.agera5_folder,
+                            .var,.statistic,
+                            .date)
+
+  agera5_brick <- raster::brick(file_path)
+  data_out <- raster::extract(agera5_brick, .location_xy)
+  date_to_extract <- gsub(x = .date, pattern = "-", replacement = ".")
+  date_to_extract <- paste0("X", date_to_extract)
+  extracted_data <- data_out[1, date_to_extract]
+
   return(extracted_data)
+
 }
 
 #'extracts data for a given location and time period
-extract_period<-function(.start_date,.end_date,.location_xy,.var,.statistic){
-  time_span<-seq.Date(from = .start_date,to = .end_date, by = "days")
-  data_out_period<-array(dim=c(1,length(time_span)))
+extract_period <- function(.agera5_folder,
+                           .start_date,
+                           .end_date,
+                           .location_xy,
+                           .var,.statistic){
+  time_span <- seq.Date(from = .start_date,
+                        to = .end_date,
+                        by = "days")
+  data_out_period <- array(dim = c(1, length(time_span)))
   for(i in 1:length(time_span)){
-    data_out_period[,i]<-extract_single_date(time_span[i],.location_xy,.var,.statistic)
+    data_out_period[, i] <- extract_single_date(.agera5_folder,
+                                                time_span[i],
+                                                .location_xy,
+                                                .var,
+                                                .statistic)
   }
-  colnames(data_out_period)<-as.character(time_span)
+
+  colnames(data_out_period) <- as.character(time_span)
+
 #273.15 is the conversion factor for Kelvin to degree Celsius
 #"Treese SA (2018) Historical Temperature. In:  History and Measurement of the Base and Derived Units.
 #Springer International Publishing, Cham, pp 837-864. doi:10.1007/978-3-319-77577-7_9"
-  data_out_period<-data_out_period-273.15
+  data_out_period <- data_out_period - 273.15
+
   return(data_out_period)
 }
 
 
 #get the the file path to extract data
-get_filepath<-function(.var,.statistic,.date_to_search){
+get_filepath <- function(.agera5_folder,
+                         .var,
+                         .statistic,
+                         .date_to_search){
   # Available temperature statistics
   #Max-24h
   #Max-Day-Time
@@ -92,37 +116,43 @@ get_filepath<-function(.var,.statistic,.date_to_search){
   #Mean-Night-Time
   #Min-24h
   #Min-Night-Time
-  if(.statistic==1)stat_code<-"Max-24h"
-  if(.statistic==2)stat_code<-"Max-Day-Time"
-  if(.statistic==3)stat_code<-"Mean-24h"
-  if(.statistic==4)stat_code<-"Mean-Day-Time"
-  if(.statistic==5)stat_code<-"Mean-Night-Time"
-  if(.statistic==6)stat_code<-"Min-24h"
-  if(.statistic==7)stat_code<-"Min-Night-Time"
+  if(.statistic == 1)stat_code <- "Max-24h"
+  if(.statistic == 2)stat_code <- "Max-Day-Time"
+  if(.statistic == 3)stat_code <- "Mean-24h"
+  if(.statistic == 4)stat_code <- "Mean-Day-Time"
+  if(.statistic == 5)stat_code <- "Mean-Night-Time"
+  if(.statistic == 6)stat_code <- "Min-24h"
+  if(.statistic == 7)stat_code <- "Min-Night-Time"
 
-  var_to_search<-.var
+  var_to_search <- .var
 
-  temp_prefix<-paste0("Temperature-Air-2m-",stat_code,"_C3S-glob-agric_AgERA5_daily_")
+  temp_prefix <- paste0("Temperature-Air-2m-", stat_code, "_C3S-glob-agric_AgERA5_daily_")
 
-  year_to_search<-format(.date_to_search, "%Y")
+  year_to_search <- format(.date_to_search, "%Y")
 
-  month_to_search<-format(.date_to_search, "%m")
+  month_to_search <- format(.date_to_search, "%m")
 
   date_pattern <- paste0(year_to_search,month_to_search)
 
-  agera5_file_pattern<-paste0(temp_prefix,date_pattern)
+  agera5_file_pattern <- paste0(temp_prefix, date_pattern)
 
-  files_<-list.files(paste(agera5_folder,var_to_search,year_to_search,sep = "/"))
-  print(date_pattern)
-  print("__________________________")
-  print(agera5_file_pattern)
-  print("__________________________")
-  print(files_)
+  files_ <- list.files(paste(.agera5_folder, var_to_search, year_to_search, sep = "/"))
+  # print(date_pattern)
+  # print("__________________________")
+  # print(agera5_file_pattern)
+  # print("__________________________")
+  # print(files_)
 
- file_name<-files_[str_detect(files_,agera5_file_pattern)]
+  file_name <- files_[str_detect(files_, agera5_file_pattern)]
   print(file_name)
- agera5_file_path<-paste(agera5_folder,var_to_search,year_to_search,file_name,sep="/")
- print(agera5_file_path)
- return(agera5_file_path)
+  agera5_file_path <- paste(.agera5_folder,
+                            var_to_search,
+                            year_to_search,
+                            file_name,
+                            sep = "/")
+
+  print(agera5_file_path)
+
+  return(agera5_file_path)
 }
 
