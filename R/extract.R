0f4e14e1b6df383bfedd859ff82a0b70dd469eb4
David Brown <50154982+dbrownf@users.noreply.github.com>
2022-03-07 17:14:14 -0600

    Update extract.R

--- R/extract.R
+++ R/extract.R
@@ -9,23 +9,37 @@
 
 #'@name ag5_extract
 #'@param variable The AgERA5 variable to extract, see details for available options
+#'@param statistic character See details for variables that require to indicate the statistic
 #'@param lon The longitude value for the location of interest
 #'@param lat The latitude value for the location of interest
 #'@param dates The dates for extracting the specified variable, either single character or a vector of length 2
 #'@param path The path for the folder containing the AgERA5 files
 #'
-#'@details
-#'@section Valid variables available to be used as \code{variable}:
+#'@details # Valid variable values to be used in \code{extract}
 #'\itemize{
-#'\item{Temperature-Air-2m}
-#'\item{Precipitation-Flux}
-#'\item{Solar-Radiation-Flux}
-#'\item{Relative-Humidity-2m}
-#'}}
+#'\item Temperature-Air-2m
+#'\item Precipitation-Flux
+#'\item Solar-Radiation-Flux
+#'\item Relative-Humidity-2m
+#'}
+#' # Valid statistics for variable "Temperature-Air-2m"
+#'\itemize{
+#'\item Max-24h
+#'\item Mean-24h
+#'\item 24_hour_minimum
+#'\item Max-Day-Time
+#'\item Mean-Day-Time
+#'\item Mean-Night-Time
+#'\item Min-Night-Time}
 #'
 #'@examples
 #'\dontrun{
-#'2m_temp <- ag5_extract(lon, lat, dates, variable, ..., path)
+#'2m_temp <- ag5_extract(lon = 35.72636,
+#'                       lat = -2.197162,
+#'                       dates = "1991-04-22",
+#'                       variable = "Temperature-Air-2m",
+#'                       statistic = "Max-Day-Time",
+#'                       path "C:/temperature_data/")
 #'}
 #'
 
@@ -43,6 +57,10 @@ ag5_extract <- function(lon, lat, dates, variable, ..., path){
 #'@export
 
 ag5_extract.default <- function(lon, lat, dates, variable, ..., path){
+
+  if(variable == "Temperature-Air-2m"){
+
+  }
 
   x <- paste(lon, lat, variable, list(...), path)
 
